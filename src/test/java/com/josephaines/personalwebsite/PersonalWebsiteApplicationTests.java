4d88f5150b360b6a667544301cbcae3c54014dc5
bushycargo <josephaines03@gmail.com>
2023-09-22 11:38:52 +0100

    First Commit.

---
+++ src/test/java/com/josephaines/personalwebsite/PersonalWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josephaines.personalwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
