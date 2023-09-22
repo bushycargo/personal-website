1de8adfb58ce74032b518fecf4f800b487e88f15
bushycargo <josephaines03@gmail.com>
2023-09-22 18:48:23 +0100

    Changed some CSS around and added js-content.css Also added js for hover colours as why not. Boilerplate screens are there for the pages to get branched off.

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', function (){
     let contentBox = document.getElementById("content-box");
 
-    let nameScreenContent = "Main Screen";
-    let aboutMeContent = "About me or something";
-    let contactContent = "Contact me brrrr";
+    let nameScreenContent = `
+        <header class="text js-h-text">Main Screen</header>
+        <p class="text js-p-text">Test p</p>
+    `;
+    let aboutMeContent = `<p class="text js-p-text">About me or something</p>`;
+    let contactContent = `<p class="text js-p-text">Contact me brrrr</p>`;
 
     contentBox.innerHTML = nameScreenContent;
+
+    // If buttons clicked
     document.getElementById("name-header").addEventListener('click', function (event) {
         contentBox.innerHTML = nameScreenContent;
     });
@@ -15,5 +20,20 @@ document.addEventListener('DOMContentLoaded', function (){
     document.getElementById("contact-header").addEventListener('click', function (event) {
         contentBox.innerHTML = contactContent;
     });
+
+    // If buttons hovered
     console.log("JS loaded");
+    document.getElementById("contact-header").addEventListener('mouseenter', function (event) {
+        event.target.style.color = "#be9afc";
+    });
+    document.getElementById("contact-header").addEventListener('mouseout', function (event) {
+        event.target.style.color = "";
+    });
+    document.getElementById("about-me-header").addEventListener('mouseenter', function (event) {
+        event.target.style.color = "#be9afc";
+    });
+    document.getElementById("about-me-header").addEventListener('mouseout', function (event) {
+        event.target.style.color = "";
+    });
+
 });
