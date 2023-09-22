58e2be2df94234c98737150f0567e38219fffed5
bushycargo <josephaines03@gmail.com>
2023-09-22 23:04:57 +0100

    Added the form to the model and linked it up to the form on the contact page.

--- src/main/java/com/josephaines/personalwebsite/controllers/WebController.java
+++ src/main/java/com/josephaines/personalwebsite/controllers/WebController.java
@@ -1,5 +1,6 @@
 package com.josephaines.personalwebsite.controllers;
 
+import com.josephaines.personalwebsite.controllers.form.Form;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class WebController {
     @GetMapping("/")
     public String index(Model model){
+        model.addAttribute("form", new Form());
         return "index";
     }
 
